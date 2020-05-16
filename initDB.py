86665505b09ec17f0a1ae5d776bd29309c034499
miguelforde <62704578+miguelforde@users.noreply.github.com>
2020-05-15 21:37:28 -0400

    Update initDB.py

--- initDB.py
+++ initDB.py
@@ -1,4 +1,15 @@
-import main
 import csv
+from main import db, Exercises, app 
 
-main.db.create_all(app=app)
+db.create_all(app=app);
+
+with open('exercises.csv', 'r') as f:
+  read = csv.reader(f)
+
+  for row in read:
+    exer = { "name": row[0], "target": row[1]}
+    exercise = Exercises(name=exer['name'], target=exer['target'])
+    db.session.add(exercise)
+    db.session.commit()
+
+print ("Database Initialised")
